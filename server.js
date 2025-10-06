d7ed7ad47868e840f99664764fce901aae9e8011
Abhixak <abhishekkumar2526105@gmail.com>
2025-10-06 13:03:27 +0530

    Added new form

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const MONGODB_URI = process.env.MONGO_URI;
 
 // Middleware
 app.use(
-  cors()
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
 );
 app.use(express.json());
 
